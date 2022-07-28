455a8a09dd0f80e0c8ccb72066f6d861a376256f
Raghav Shankar <raghav@deepsource.io>
2022-07-28 09:22:15 +0530

    try refreshing issues detected

--- java/web/src/main/java/com/example/server/Server.java
+++ java/web/src/main/java/com/example/server/Server.java
@@ -5,6 +5,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.*;
 
 public class Server extends HttpServlet {
@@ -28,15 +34,31 @@ public class Server extends HttpServlet {
             s.execute("SELECT userName, isWin FROM users WHERE uid = " + ticketNumber + ";");
             ResultSet r = s.getResultSet();
 
+            URI offerAPI = new URI(req.getParameter("offerAPI"));
+
             String id = r.getString(0);
 
             if (r.getBoolean("isWin") && b) {
                 resp.getWriter().print("You win, " + r.getString("userName") + "!<br>You can fill your details in with this link: " + req.getParameter(id));
+                StringBuffer sb = new StringBuffer();
+                HttpClient hc = HttpClient.newBuilder().build();
+                HttpRequest offerReq = HttpRequest.newBuilder(offerAPI).GET().build();
+                HttpResponse<String> offerResp = hc.send(offerReq, HttpResponse.BodyHandlers.ofString());
+
+                if (offerResp.body() != null) {
+                    resp.getWriter().write("An additional offer is available only for you!");
+                }
+
+
+
+                resp.getWriter().write("You win, " + r.getString("userName") + "!<br>You can fill your details in with this link: " + req.getParameter(id));
             } else {
                 resp.getWriter().print("You lose, " + r.getString("userName"));
             }
         } catch (SQLException throwable) {
             throwable.printStackTrace();
+        } catch (URISyntaxException e) {} catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         resp.setStatus(200);
     }

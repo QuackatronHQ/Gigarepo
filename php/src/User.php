c9eb6cb0de40aaa887485a267d1cfac942da01d9
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2022-12-15 16:31:39 +0530

    Update php/src/User.php
    Signed-off-by: Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>

--- php/src/User.php
+++ php/src/User.php
@@ -23,10 +23,10 @@ class User
 }
 
 $conn = 'db-connect';
-​
+
 $offset = $argv[0]; // beware, no input validation!
 $query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
 $result = pg_query($conn, $query);
-​
+
 $sql = "SELECT * FROM users WHERE id = " . $_GET['id'];
 $result = pg_query($conn, $sql);

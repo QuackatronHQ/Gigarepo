05a2b79f8ac65166631125b033ebee73321d707b
AKSHAYA MADHURI <76612327+akshaya-hub@users.noreply.github.com>
2023-02-09 16:52:21 +0530

    update calculator.py
    added log functions to the calculator
    
    Signed-off-by: AKSHAYA MADHURI <76612327+akshaya-hub@users.noreply.github.com>

--- python/calculator.py
+++ python/calculator.py
@@ -1,17 +1,21 @@
 """Simple calculator app"""
 
+import math
 
 def main():
     while True:
         try:
             string = input("> ")
-            print(eval(string))
+            parts = string.split(" ")
+            if len(parts) == 3 and parts[0] == "log":
+                print(math.log(int(parts[2]), int(parts[1])))
+            else:
+                print(eval(string))
         except (KeyboardInterrupt, EOFError):
             print()
             exit()
-        except (BaseException, KeyboardInterrupt) as e:
+        except Exception as e:
             print(e)
-
 
 if __name__ == "__main__":
     print("Welcome to calculator!")

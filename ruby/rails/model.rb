28bcf2a8f84e294af344377a82c0cb7bbcb1bc2f
Syed Faraaz Ahmad <faraaz@deepsource.io>
2023-03-07 12:46:21 +0530

    add activerecord

--- ruby/rails/model.rb
+++ ruby/rails/model.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   attribute :confirmed_at, :datetime, default: Time.now
 end
+
+class Person < ApplicationRecord
+  after_commit :after_commit_callback
+  before_validation :before_validation_callback
+
+  validates :name, presence: true
+end
